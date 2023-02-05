f6b0f123c0b82b562e6e51e9139f63727448172a
Purnima Surve <surve.purnima@gmail.com>
2023-02-05 00:01:42 -0500

    win/lose condition

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,93 +1,10 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.EventSystems;
-
-//public class Move : MonoBehaviour
-//{
-//    private Vector2 moveDirection;
-//    public Rigidbody2D rb;
-//    public float rotateSpeed = 80.0f;
-//    public float acceleration = 20;
-//    public float decceleration = 5;
-//    public float currentSpeed = 20;
-//    public float maxSpeed = 100;
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        //if (Input.GetKey(KeyCode.DownArrow)) {
-//        //    processInputs();
-//        //    moveWithAcceleration();
-//        //}
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        processInputs();
-//        moveWithAcceleration();
-//    }
-//    private void FixedUpdate()
-//    {
-
-//    }
-//    void processInputs()
-//    {
-//        //driving forward and backward using user input
-//        float moveX = Input.GetAxis("Horizontal");
-//        float moveY = Input.GetAxis("Vertical");
-//        moveDirection = new Vector2(moveX, moveY);
-//    }
-//    void moveWithAcceleration() {
-
-//        currentSpeed =  acceleration * Time.deltaTime;
-//        //this.transform.Translate(moveDirection * currentSpeed * Time.deltaTime);
-
-//        //currentSpeed += forwardinput * acceleration * Time.deltaTime;
-//        if (currentSpeed > maxSpeed)
-//        {
-//            currentSpeed = maxSpeed;
-//        }
-//        if (currentSpeed < -maxSpeed)
-//        {
-//            currentSpeed = -maxSpeed;
-//        }
-
-//        //if (moveDirection.y == 0)
-//        //{
-//        //    if (currentSpeed > 0)
-//        //    {
-//        //        currentSpeed -= decceleration * Time.deltaTime;
-//        //    }
-//        //}
-
-//        //{
-//        //    if (currentSpeed > 0)
-//        //        currentSpeed -= decceleration * Time.deltaTime;
-
-//        //}
-//        rb.velocity = new Vector2(moveDirection.x * currentSpeed, moveDirection.y * currentSpeed);
-
-//        //this.transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
-
-
-//        ////steer the car to the left and right using user input
-//        //float rotateinput = Input.GetAxis("Horizontal");
-//        //rotateinput = rotateinput * rotateSpeed * Time.deltaTime;
-//        //this.transform.Rotate(0, rotateinput, 0);
-//    }
-//    //private void OnCollisionEnter2D(Collision2D collision)
-//    //{
-//    //    rb.AddForce(-moveDirection * currentSpeed, ForceMode2D.Impulse);
-//    //}
-
-//}
 
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Move : MonoBehaviour
 {
@@ -98,11 +15,12 @@ public class Move : MonoBehaviour
 
     //direction the root is traveling
     private Vector2 direction;
-
+    public TextMeshProUGUI Win;
     void Start()
     {
         //Initialize the root going downward
         direction = Vector2.down;
+        Win.text = " ";
     }
 
     // Player control for moving the root
@@ -116,7 +34,7 @@ public class Move : MonoBehaviour
         {
             direction.x += TurnSpeed;
         }
-
+        
     }
     //Late update function to update root position
     private void LateUpdate()
@@ -140,6 +58,10 @@ public class Move : MonoBehaviour
         if (collision.tag == "powerup")
         {
             Speed += 0.5f;
+        }
+        if (collision.tag == "dest") {
+            Speed = 0;
+            Win.text = "YOU WIN !! ";
         }
     }
 }
